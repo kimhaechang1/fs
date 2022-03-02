a06ead5569b22c1937defc8ec739a5ec765fa96a
kimhaechang1 <81299056+kimhaechang1@users.noreply.github.com>
2022-03-02 16:39:51 +0900

    data flow & axios & proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { User } = require('./models/Users') // 모델 불러오기
@@ -23,6 +23,10 @@ mongoose.connect(config.mongoURI,{
 .catch(err => console.log(err))
 
 app.get('/',(req,res) => res.send('hello world'))
+
+app.get('/api/hello',(req,res)=>{
+    res.send("안녕하세요")
+})
 
 app.post('/api/users/register',(req,res)=>{
     // 회원가입 할 때 필요한 정보들을 client에서 가져오면
