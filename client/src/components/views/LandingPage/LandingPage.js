7371b5b80b92654df7827885516a1564a9a02f81
kimhaechang1 <81299056+kimhaechang1@users.noreply.github.com>
2022-04-26 17:38:36 +0900

    20220426

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, {useEffect}  from 'react'
 import axios from 'axios'
+import {DatePicker} from 'antd'
 function LandingPage(props) {
    useEffect(()=>{
        axios.get('/api/hello')
@@ -7,7 +8,7 @@ function LandingPage(props) {
    },[])
     return (
         <div>
-            랜딩페이지
+            <DatePicker/>
         </div>
     );
 }
